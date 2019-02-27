aea3f4f5fd288c049ed3b6d51a6f18af2b8ec6f1
Devtest <Devtest@L-1F4EB8>
2019-02-28 08:33:34 +1300

    Add code for agile exercises

--- cs/src/Hangman.cs
+++ cs/src/Hangman.cs
@@ -1,83 +1,94 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Linq;
-using System.Text;
-using Newtonsoft.Json.Linq;
 
 namespace TddExMicrotest
 {
-    public enum GameStatus { NotStarted, Guessing, Winner, Hanged };
-    public enum LetterStatus { NotGuessed, GuessedCorrect, GuessedIncorrect };
+	public enum GameStatus { NotStarted, Guessing, Winner, Hanged };
 
-    public class Letter {
-        public char Value;
-        public LetterStatus Status;
-        public Letter(char value, LetterStatus status){ 
-            Value = value;
-            Status = status;
-        }
-    }
+	public enum LetterStatus { NotGuessed, GuessedCorrect, GuessedIncorrect };
 
-    public class Hangman
-    {
-        private static Random R = new Random();
-        private const int DEATH_LIMIT = 11;
-        public readonly string _word;
-        private Letter[] _letters = Enumerable.Range('A', 26).Select(x => new Letter((char)x, LetterStatus.NotGuessed)).ToArray();
+	public class Letter
+	{
+		public char Value;
+		public LetterStatus Status;
 
-        public Hangman(string word){
-            Console.WriteLine("Hangman started with word: " + word);
-            _word = word;
-            Status = GameStatus.NotStarted;
-        }
+		public Letter(char value, LetterStatus status)
+		{
+			Value = value;
+			Status = status;
+		}
+	}
 
-        public static Hangman FromInternet(int length) {
-            TheInternet internet = new TheInternet();
-            Console.WriteLine("Starting hangman with a word of length " + length);
-            var content = internet.Get(GameUrls.WORDS, length);
-            var words = JArray.Parse(content);
-            var index = R.Next(0, words.Count);
-            var word = words[index].Value<string>("word");
-            return new Hangman(word);
-        }
+	public class Hangman
+	{
+		private static Random R = new Random();
+		private const int DEATH_LIMIT = 11;
+		private Letter[] _letters = Enumerable.Range('A', 26).Select(x => new Letter((char)x, LetterStatus.NotGuessed)).ToArray();
 
-        public GameStatus Status{ get; private set; }
+		public readonly string _word;
 
-        public int NumberLetters{get{ return this._word.Length; }}
+		public Hangman(string word)
+		{
+			Console.WriteLine("Hangman started with word: " + word);
+			_word = word;
+			Status = GameStatus.NotStarted;
+		}
 
-        public int NumberWrongGuesses{
-            get{
-                return this._letters.Where(x => x.Status == LetterStatus.GuessedIncorrect).Count();
-            }
-        }
+		public static Hangman FromInternet(int length, ITheInternet theInternet)
+		{
+			Console.WriteLine("Starting hangman with a word of length " + length);
+			var content = theInternet.Get(GameUrls.WORDS, length);
+			var words = JArray.Parse(content);
+			var index = R.Next(0, words.Count);
+			var word = words[index].Value<string>("word");
+			return new Hangman(word);
+		}
 
-        public int NumberRightGuesses{
-            get{
-                var letters = _word.ToCharArray();
-                var letterStatuses = letters.Select( i => _letters[(i - 'a')]);
-                return letterStatuses.Where(x => x.Status == LetterStatus.GuessedCorrect).Count();
-            }
-        }
+		public GameStatus Status { get; private set; }
 
-        public bool Guess(char letter){
-            Console.WriteLine("Guessing " + letter);
-            if( Status != GameStatus.NotStarted && Status != GameStatus.Guessing) {
-                throw new System.ApplicationException("Game not in state that can accept a guess.");
-            }
-            Status = GameStatus.Guessing;
-            var isCorrect = _word.Contains(letter);
-            _letters[(letter - 'a')].Status = (isCorrect ? LetterStatus.GuessedCorrect : LetterStatus.GuessedIncorrect);
-            if( NumberWrongGuesses >= DEATH_LIMIT){
-                Console.WriteLine("Hanged!");
-                Status = GameStatus.Hanged;
-            }
-            if( NumberRightGuesses >= NumberLetters ){
-                Console.WriteLine("Winner!");
-                Status = GameStatus.Winner;
-            }
-            Console.WriteLine("Letter is correct? " + isCorrect);
-            return isCorrect;
-        }
-    }
+		public int NumberLetters => this._word.Length;
+
+		public int NumberWrongGuesses
+		{
+			get
+			{
+				return _letters.Count(x => x.Status == LetterStatus.GuessedIncorrect);
+			}
+		}
+
+		public int NumberRightGuesses
+		{
+			get
+			{
+				var letters = _word.ToCharArray();
+				var letterStatuses = letters.Select(i => _letters[(i - 'a')]);
+				return letterStatuses.Count(x => x.Status == LetterStatus.GuessedCorrect);
+			}
+		}
+
+		public bool Guess(char letter)
+		{
+			Console.WriteLine("Guessing " + letter);
+			if (Status != GameStatus.NotStarted && Status != GameStatus.Guessing)
+			{
+				throw new ApplicationException("Game not in state that can accept a guess.");
+			}
+			Status = GameStatus.Guessing;
+			var isCorrect = _word.Contains(letter);
+			_letters[(letter - 'a')].Status = (isCorrect ? LetterStatus.GuessedCorrect : LetterStatus.GuessedIncorrect);
+			if (NumberWrongGuesses >= DEATH_LIMIT)
+			{
+				Console.WriteLine("Hanged!");
+				Status = GameStatus.Hanged;
+			}
+			if (NumberRightGuesses >= NumberLetters)
+			{
+				Console.WriteLine("Winner!");
+				Status = GameStatus.Winner;
+			}
+			Console.WriteLine("Letter is correct? " + isCorrect);
+			return isCorrect;
+		}
+	}
 }
--- cs/tests/UnitTest1.cs
+++ cs/tests/UnitTest1.cs
@@ -1,16 +1,93 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using TddExMicrotest;
+using Moq;
+using System;
+using System.Net;
 
 namespace TddExMicrotest.Tests
 {
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void ThisTestIsNotMicro()
-        {
-            var x = Hangman.FromInternet(5);
-            Assert.AreEqual(6, x.NumberLetters);
-        }
-    }
+	public class FakeTheInternet : ITheInternet
+	{
+		private readonly string _result;
+
+		public FakeTheInternet(string result)
+		{
+			_result = result;
+		}
+
+		public string Get(string url, params object[] parameters)
+		{
+			return "[{ word: \"" + _result + "\" }]";
+		}
+	}
+
+	public class FakeBrokenTheInternet : ITheInternet
+	{
+		private readonly Exception _exception;
+
+		public FakeBrokenTheInternet(Exception exception)
+		{
+			_exception = exception;
+		}
+
+		public string Get(string url, params object[] parameters)
+		{
+			throw _exception;
+		}
+	}
+
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void SimpleWordOfCorrectLength()
+		{
+			// Arrange
+			var theInternet = new FakeTheInternet("hello");
+
+			// Act
+			var x = Hangman.FromInternet(5, theInternet);
+
+			// Assert
+			Assert.AreEqual(5, x.NumberLetters);
+		}
+
+		[TestMethod]
+		public void HandlesEmptyStringFromTheInternet()
+		{
+			// Arrange
+			var theInternet = new FakeTheInternet(string.Empty);
+
+			// Act
+			var x = Hangman.FromInternet(5, theInternet);
+
+			// Assert
+			Assert.AreEqual(0, x.NumberLetters);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(WebException))]
+		public void HandlesConnectFailureWebException()
+		{
+			// Arrange
+			var theInternet = new FakeBrokenTheInternet(new WebException("ConnectFailure", WebExceptionStatus.ConnectFailure));
+
+			// Act
+			var hangman = Hangman.FromInternet(5, theInternet);
+		}
+
+		[TestMethod]
+		public void MoqInternet()
+		{
+			// Arrange
+			var mock = new Mock<ITheInternet>();
+			mock.Setup(x => x.Get(It.IsAny<string>(), It.IsAny<int>()))
+				.Returns("[{ word: \"hello\" }]");
+
+			// Act
+			var hangman = Hangman.FromInternet(5, mock.Object);
+
+			// Assert
+			Assert.AreEqual(5, hangman.NumberLetters);
+		}
+	}
 }
